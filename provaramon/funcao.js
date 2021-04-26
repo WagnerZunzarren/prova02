f429f30571b2ad3efb9ec024a6c9de082813dd2a
WagnerZunzarren <80364344+WagnerZunzarren@users.noreply.github.com>
2021-04-25 23:44:10 -0300

    Add files via upload

---
+++ provaramon/funcao.js
@@ -0,0 +1,22 @@
+var ul;
+var imput;
+var button;
+
+function clicarBotao() {
+    var button = document.querySelector('button');
+}
+
+var lista = document.createElement('li');
+var span = document.createElement('span');
+var button = documento.createElement('Excluir');
+
+span.appendChild(lista);
+button.appendChild(lista);
+
+const handleClickWithIf = event => {
+    if (!event.ctrlkey && !event.metaKey){
+        event.preventDefault();
+        display.log('')
+    }
+}
+element.focus()
